d7c13fee8b5d3ce2aeeac49fabbdee9e2e11785c
jzuriaga <jzuriaga@redhat.com>
2019-02-18 06:58:38 +0100

    Add Microservice testing.

---
+++ src/test/java/com/redhat/training/juanzu/helloworld/HelloControllerTest.java
@@ -0,0 +1,39 @@
+package com.redhat.training.juanzu.helloworld;
+
+import static org.junit.Assert.assertEquals;
+import java.net.URL;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.swarm.arquillian.DefaultDeployment;
+import org.wildfly.swarm.arquillian.DefaultDeployment.Type;
+
+@RunWith(Arquillian.class)
+@DefaultDeployment(type = Type.WAR)
+public class HelloControllerTest {
+
+    @ArquillianResource
+    private URL url;
+
+    @Test
+    @RunAsClient
+    public void testGet () {
+        WebTarget client = getWebTarget("data/hello");
+        Response response = client.request(MediaType.TEXT_PLAIN).get();
+        assertEquals(200, response.getStatus());
+        assertEquals("Hello World", response.readEntity(String.class));
+    }
+
+    private WebTarget getWebTarget (final String endpoint) {
+        final Client client = ClientBuilder.newBuilder().build();
+        return client.target(this.url.toExternalForm() + endpoint);
+    }
+
+}
